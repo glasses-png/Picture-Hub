6e5c1a0acf6d536c309b40a50d5fac804708ad36
glasses-png <cchelangat16@gmail.com>
2020-07-31 09:46:50 +0300

    added index,imagelocation and search results in views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Image, Location
 
 # Create your views here.
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'pic/index.html', {'images': images, 'locations': locations})
+
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'pic/location.html', {'location_images': images})
+
+
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pic/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "Sorry! You haven't searched for any image category"
+        return render(request, 'pic/search_results.html', {"message": message})
+
